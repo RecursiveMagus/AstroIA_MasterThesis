7ad8b7d9cdb691a6494000b63ddf6c55fd1943e1
Vogelmann <idepalau@gmail.com>
2023-05-29 11:57:26 +0200

    Added memory images

---
+++ Part2_Poincare/PlotResults.m
@@ -0,0 +1,21 @@
+function PlotResults(real_points, prediction_points, is_progresor)
+
+    x0=10;
+    y0=10;
+    width=1400;
+    height=300 * 3 +50;
+    set(gcf,'position',[x0,y0,width,height]);
+
+    if is_progresor
+        plot(real_points(:,1), real_points(:,2), '.', MarkerSize = 1, Color = 'blue'); % Actual points
+        hold on;
+        plot(prediction_points(:,1), prediction_points(:,2), '.', MarkerSize = 1,  Color = 'red'); % Predictions
+    else
+        plot(real_points(:,5), real_points(:,6), '.', MarkerSize = 1, Color = 'blue'); % Actual points
+        hold on;
+        plot(prediction_points(:,1), prediction_points(:,2), '.', MarkerSize = 1,  Color = 'red'); % Predictions
+    end
+
+
+end
+
